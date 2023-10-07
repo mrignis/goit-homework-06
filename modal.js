b9a4fba877c0caa07f23f7b981a1e55837cb603c
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2023-10-07 14:53:35 -0400

    up

--- modal.js
+++ modal.js
@@ -5,3 +5,12 @@ document.getElementById("modal-open-btn").addEventListener("click", function () 
 document.getElementById("modal-close-order-btn").addEventListener("click", function () {
 	document.getElementById("modal-order").classList.remove("is-open")
 })
+
+let menuBtn = document.querySelector('.menu-btn');
+let headerMenu = document.querySelector('.header-menu');
+menuBtn.addEventListener('click', function () {
+    headerMenu.classList.toggle('active');
+});
+
+
+
