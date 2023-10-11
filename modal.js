0c1de2f431e75533ad9fb65e7a53835250486984
PYLYP CHELBARAKH <143128807+mrignis@users.noreply.github.com>
2023-10-11 01:57:54 -0400

    upgrade

--- modal.js
+++ modal.js
@@ -11,6 +11,24 @@ let headerMenu = document.querySelector('.header-menu');
 menuBtn.addEventListener('click', function () {
     headerMenu.classList.toggle('active');
 });
+/*хедер  */
+// Отримуємо посилання на елементи DOM
+const modal = document.getElementById('myModal'); // Ваш ID модального вікна
+const openModalButton = document.getElementById('openModalButton'); // Ваш ID кнопки для відкриття модального вікна
+const closeModalButton = document.getElementById('modal-hover-close-order-btn'); // Ваш ID кнопки для закриття модального вікна
 
+// Функція для відкриття модального вікна
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Функція для закриття модального вікна
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Додаємо обробники подій для відкриття та закриття
+openModalButton.addEventListener('click', openModal);
+closeModalButton.addEventListener('click', closeModal);
 
 
